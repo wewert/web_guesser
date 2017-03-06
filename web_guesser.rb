f7b1ecb0f4d8f44655594ceb9d6c43b881a9aedb
Ken Lee <wewert@gmail.com>
2017-03-06 00:32:40 -0700

    I3: number of guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
-number = rand(101)
+SECRET_NUMBER = rand(101)
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+end
+
+def check_guess(guess)
+  if guess - SECRET_NUMBER > 5
+    "Your guess #{guess} is WAY TOO HIGH!"
+  elsif SECRET_NUMBER - guess > 5
+    "Your guess #{guess} is WAY TOO LOW!"
+  elsif guess > SECRET_NUMBER
+    "Your guess is TOO HIGH."
+  elsif guess < SECRET_NUMBER
+    "Your guess is TOO LOW."
+  else
+    "You have chosen wisely...that is the correct cup with the number #{SECRET_NUMBER} on it."
+  end
 end
